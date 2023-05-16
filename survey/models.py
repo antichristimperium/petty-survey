a5dd267b0a127c484f7194dbf5096fd7f247359c
antichristimperium <109247759+antichristimperium@users.noreply.github.com>
2023-05-16 15:33:32 -0600

    Fix empty lines

--- survey/models.py
+++ survey/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
-
 class TimeStamped(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
